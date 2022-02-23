361d5a3f36c3cb2c938c0344fd293256bf510ffe
beerer <2426758235@qq.com>
2022-02-23 15:19:26 +0800

    last

--- books(仅结构).sql
+++ books(仅结构).sql
@@ -5,17 +5,28 @@
  Source Server Type    : MySQL
  Source Server Version : 50650
  Source Host           : 1.117.231.72:3306
- Source Schema         : books
+ Source Schema         : book
 
  Target Server Type    : MySQL
  Target Server Version : 50650
  File Encoding         : 65001
 
- Date: 18/10/2021 21:19:20
+ Date: 23/02/2022 15:18:27
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for administrators
+-- ----------------------------
+DROP TABLE IF EXISTS `administrators`;
+CREATE TABLE `administrators`  (
+  `adm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `adm_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `adm_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`adm_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for book_details
@@ -30,7 +41,7 @@ CREATE TABLE `book_details`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `book_id_index`(`book_id`) USING BTREE,
   INDEX `sort_id_index`(`sort_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1040720 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1040718 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for book_infos
@@ -52,5 +63,32 @@ CREATE TABLE `book_infos`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `book_id_index`(`book_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1007 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_mail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `signin` int(11) NULL DEFAULT NULL,
+  `dlevel` int(11) NULL DEFAULT NULL,
+  `last_signed` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for verification
+-- ----------------------------
+DROP TABLE IF EXISTS `verification`;
+CREATE TABLE `verification`  (
+  `verification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `verification_mail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `verification_random` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `verification_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`verification_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
